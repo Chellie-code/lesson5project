df7624fc0ce912f91317a9a34593973d2de74814
Chellie-code <mimirekon9@gmail.com>
2022-03-21 12:34:06 +0300

    added text

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let diameterInput = prompt('Enter diameter of circle');
-let radiusInput = propmt('Enter radiu of circle');
 
-function calculateArea(diameterInput,radiusInput){
-   let area = diameter * radiusInput;
-   alert(`Area of your circle is ${area}`);
+
+function calculateArea(){
+   let area = 3.14*(diameterInput/2)**2
+   alert(`Area of your circle is ${area}`)
    //alert('Area of your circle is' + area)
 }
 
-calculateArea(diameterInput,radiusInput);
+calculateArea();
